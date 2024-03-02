0958eb615d0541df9ca93ff4937ea38d9e4c24c1
armpro908 <armpro908@yandex.ru>
2024-03-02 22:40:29 +0700

    update

--- xml_parser.ps1
+++ xml_parser.ps1
@@ -20,25 +20,18 @@ function GetXmlFiles($folderPath) {
 function ProcessXmlFile($file) {
     $xmlContent = [System.Xml.XmlDocument](Get-Content -Path $file.FullName)
     $cadNodes = $xmlContent.SelectNodes('//CAD')
-    $panelNode = $xmlContent.SelectSingleNode('//PANEL')
 
     $quantity = 1
     if ($file.Name -match '--(\d+)\.XML$') {
         $quantity = [int]$matches[1]
     }
 
-    $panelLength = [float]$panelNode.PanelLength
-    $panelWidth = [float]$panelNode.PanelWidth
-
     $foundHingeInFile = $false
     $totalCount = 0
     $count35 = 0
-    $totalLineLength = 0
-    $blindLineLength = 0
-    $throughLineLength = 0
 
     foreach ($cadNode in $cadNodes) {
-        $totalCount, $count35, $totalLineLength, $blindLineLength, $throughLineLength = ProcessCadNode -CadNode $cadNode -Quantity $quantity -TotalCount $totalCount -Count35 $count35 -TotalLineLength $totalLineLength -BlindLineLength $blindLineLength -ThroughLineLength $throughLineLength -PanelLength $panelLength -PanelWidth $panelWidth
+        $totalCount, $count35 = ProcessCadNode -CadNode $cadNode -Quantity $quantity -TotalCount $totalCount -Count35 $count35
 
         if ($count35 -gt 0) {
             $foundHingeInFile = $true
@@ -48,15 +41,12 @@ function ProcessXmlFile($file) {
     return [PSCustomObject]@{
         TotalCount = $totalCount
         Count35 = $count35
-        TotalLineLength = $totalLineLength
-        BlindLineLength = $blindLineLength
-        ThroughLineLength = $throughLineLength
         FoundHingeInFile = $foundHingeInFile
         FileName = $file.Name
     }
 }
 
-function ProcessCadNode($CadNode, $Quantity, $TotalCount, $Count35, $TotalLineLength, $BlindLineLength, $ThroughLineLength, $PanelLength, $PanelWidth) {
+function ProcessCadNode($CadNode, $Quantity, $TotalCount, $Count35) {
     $diameterNodes = $CadNode.SelectNodes('.//Diameter')
     $TotalCount += $diameterNodes.Count * $Quantity
 
@@ -70,33 +60,7 @@ function ProcessCadNode($CadNode, $Quantity, $TotalCount, $Count35, $TotalLineLe
         }
     }
 
-    $typeNoNode = $CadNode.SelectSingleNode('.//TypeNo')
-    if ($typeNoNode -and $typeNoNode.'#text' -eq "3") {
-        $beginX = [int]$CadNode.SelectSingleNode('.//BeginX').'#text'
-        $beginY = [int]$CadNode.SelectSingleNode('.//BeginY').'#text'
-        $endX = [int]$CadNode.SelectSingleNode('.//EndX').'#text'
-        $endY = [int]$CadNode.SelectSingleNode('.//EndY').'#text'
-
-        if ($beginX -eq $endX) {
-            $line_length = [Math]::Abs($endY - $beginY)
-            if (($beginY -eq 0 -or $beginY -eq $panelWidth) -and ($endY -eq 0 -or $endY -eq $panelWidth)) {
-                $ThroughLineLength += $line_length * $Quantity
-            } else {
-                $BlindLineLength += $line_length * $Quantity
-            }
-        } elseif ($beginY -eq $endY) {
-            $line_length = [Math]::Abs($endX - $beginX)
-            if (($beginX -eq 0 -or $beginX -eq $panelLength) -and ($endX -eq 0 -or $endX -eq $panelLength)) {
-                $ThroughLineLength += $line_length * $Quantity
-            } else {
-                $BlindLineLength += $line_length * $Quantity
-            }
-        }
-
-        $TotalLineLength += $line_length * $Quantity
-    }
-
-    return $TotalCount, $Count35, $TotalLineLength, $BlindLineLength, $ThroughLineLength
+    return $TotalCount, $Count35
 }
 
 $folderPath = SelectFolder
@@ -110,11 +74,7 @@ foreach ($file in $files) {
 
 $totalCount = ($results | Measure-Object -Property TotalCount -Sum).Sum
 $count35 = ($results | Measure-Object -Property Count35 -Sum).Sum
-$totalLineLength = ($results | Measure-Object -Property TotalLineLength -Sum).Sum
-$blindLineLength = ($results | Measure-Object -Property BlindLineLength -Sum).Sum
-$throughLineLength = ($results | Measure-Object -Property ThroughLineLength -Sum).Sum
 $filesWithHinges = ($results | Where-Object { $_.FoundHingeInFile }).FileName
-
 
 Write-Host "==============================================================="
 Write-Host "                         Отчет                                 "
@@ -132,17 +92,7 @@ Write-Host $($totalCount - $count35) -ForegroundColor DarkRed
 Write-Host "Файлы с петлями: " -NoNewline
 Write-Host $($filesWithHinges -join ', ') -ForegroundColor DarkRed
 
-Write-Host "Суммарная длина пазов (в метрах): " -NoNewline
-Write-Host $([Math]::Round($totalLineLength / 1000, 2)) -ForegroundColor DarkRed
-
-Write-Host "Глухие пазы: " -NoNewline
-Write-Host $([Math]::Round($blindLineLength / 1000, 2)) -ForegroundColor DarkRed
-
-Write-Host "Сквозные пазы: " -NoNewline
-Write-Host $([Math]::Round($throughLineLength / 1000, 2)) -ForegroundColor DarkRed
-
 Write-Host "==============================================================="
-
 
 pause
 
